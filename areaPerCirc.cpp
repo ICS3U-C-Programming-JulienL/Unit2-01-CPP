bfb27c791eb32ad72f6869f680c1876f2ca18850
Julien Lamoureux <julien.lamoureux@stu.ocsb.ca>
2023-09-22 15:02:24 +0000

    Updated areaPerCirc.cpp

--- areaPerCirc.cpp
+++ areaPerCirc.cpp
@@ -8,11 +8,11 @@
 #include <iostream>
 
 int main() {
-    float radius = 3;
+    float radius = 3.5;
     float circumference = 2 * M_PI * radius;
     float areaOfCircle = M_PI * pow(radius, 2);
     std ::cout << "A circle that has a radius of :" << std:: endl;
-    std ::cout << "3 cm" << std::endl;
+    std ::cout << "3.5 cm" << std::endl;
     std ::cout << "" << std::endl;
     std ::cout << "The Area = " << areaOfCircle << " cm^2" << std::endl;
     std ::cout << "The Circumference = " << circumference << " cm" << std::endl;
